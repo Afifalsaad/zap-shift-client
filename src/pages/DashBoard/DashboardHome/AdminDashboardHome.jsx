638a64766667156251f5c85eac324e3846bc366a
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-12-02 17:04:47 +0600

    pipelineline aggregation added

--- src/pages/DashBoard/DashboardHome/AdminDashboardHome.jsx
+++ src/pages/DashBoard/DashboardHome/AdminDashboardHome.jsx
@@ -1,9 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AdminDashboardHome = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: deliveryStat = [] } = useQuery({
+    queryKey: ["delivery-status-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/parcels/delivery-status/stats");
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
   return (
     <div>
       <h2 className="text-4xl text-secondary font-bold">Admin Dashboard</h2>
+
+      {/* Stats */}
+      <div className="max-w-11/12 mx-auto mt-10">
+        <div className="stats shadow">
+          {deliveryStat.map((stat) => (
+            <div className="stat place-items-center">
+              <div className="stat-title">{stat._id}</div>
+              <div className="stat-value">{stat.count}</div>
+              <div className="stat-desc">From January 1st to February 1st</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
